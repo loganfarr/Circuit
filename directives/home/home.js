743390a0abec89c2a987e296e0035bfde56b5c23
Logan Farr <logan@loganfarr.com>
2015-07-25 15:54:45 -0600

    Worked some more on the backend service and HomeController. Also added a favicon

--- directives/home/home.js
+++ directives/home/home.js
@@ -1,23 +1,23 @@
 (function() {
-	var HomeController = function($scope, $routeParams, backend, $log) {
+	var HomeController = function($scope, $routeParams, backend, $log, $q) {
 		var onRecentProjects = function(data) {
 			$scope.recentProjects = data;
 		}
 
 		var onRecentBlogPosts = function(data) {
 			$scope.recentBlogPosts = data;
+			$log.info($scope.recentBlogPosts);
+			$log.info(JSON.stringify($scope.recentBlogPosts));
 		}
 
 		var onError = function(response) {
 			$('.error-container').html("<div class='messages error'>Could not fetch the data. <br /><br />More information: <br />"+response+"</div>");
 		}
 
-		// backend.getRecentProjects.$promise.then(onRecentProjects, onError);
-		var recentProjectData = backend.getRecentProjects;
-		// backend.getRecentBlogPosts.$promise.then(onRecentBlogPosts, onError);
-		var recentBlogPostData = backend.getRecentBlogPosts;
-		$log.info(JSON.stringify(recentProjectData));
-		$log.info(JSON.stringify(recentBlogPostData));
+		$q.when(backend.getRecentProjects).then(onRecentProjects, onError);
+		$q.when(backend.getRecentBlogPosts).then(onRecentBlogPosts, onError);
+
+
 	};
 	var app = angular.module("loganfarr");
 	app.controller("HomeController", HomeController);
--- services/backend.js
+++ services/backend.js
@@ -1,24 +1,59 @@
 (function() {
-	var backend = function($http) {
+	var backend = function($http, $q) {
+		var fetchData = function(url) {
+			return $http.get(url)
+						.then(
+							//Success Function
+							function(response) {
+								$log.info("Success function fired");
+								if(typeof response.data === 'object') {
+									return response.data;
+								}
+								else {
+									//Invalid response
+									return $q.reject(response.data);
+								}
+							},
+							//Error function
+							function(response) {
+								$log.error("Error function fired")
+								//Something went wrong
+								return $q.reject(response.data);
+							}
+						);
+		};
+
 		var getRecentProjects = function() {
-			return $http.get("http://backend.loganfarr.com/recent-portfolio.json")
-						.then(function(response) { return response.data; });
-		}
+			// return $http.get("http://backend.loganfarr.com/recent-portfolio.json");;;
+			// $log.info("Getting recent projects");
+			// return fetchData("http://backend.loganfarr.com/recent-portfolio.json");
+
+			return $http.get("http://backend.loganfarr.com/recent-portfolio")
+						.then(function(response) {
+							return response.data;
+						});
+		};
 
 		var getProjects = function() {
-			return $http.get("http://backend.loganfarr.com/portfolio.json")
-						.then(function(response) { return response.data; });
-		}
+			// return $http.get("http://backend.loganfarr.com/portfolio.json")
+			// 			.then(function(response) { return response.data; });
+		
+			return fetchData("http://backend.loganfarr.com/portfolio");
+		};
 
 		var getRecentBlogPosts = function() {
-			return $http.get("http://backend.loganfarr.com/recent-blog.json")
-						.then(function(response) { return response.data; });
-		}
+			// return $http.get("http://backend.loganfarr.com/recent-blog.json")
+			// 			.then(function(response) { return response.data; });
+
+			return fetchData("http://backend.loganfarr.com/recent-blog");
+		};
 
 		var getBlogPosts = function() {
-			return $http.get("http://backend.loganfarr.com/blog.json")
-						.then(function(response) { return response.data; });
-		}
+			// return $http.get("http://backend.loganfarr.com/blog.json")
+			// 			.then(function(response) { return response.data; });
+
+			return fetchData("http://backend.loganfarr.com/blog");
+		};
 
 		return {
 			getRecentProjects: 	getRecentProjects,
