3d80d841313471e8d76f2ce3a02dd15fc1dcc23c
Logan Farr <Logan@loganfarr.com>
2015-09-30 22:09:45 -0600

    Got the 'Read Full Post' functionality working. Started on the 'Show Less' part

--- controllers/home/home.js
+++ controllers/home/home.js
@@ -6,20 +6,34 @@
 
 		var onRecentBlogPosts = function(data) {
 			$scope.recentBlogPosts = data;
+
+			angular.forEach(data, function(post, key) {
+				post.shownMore = false;
+				post.body = $sce.trustAsHtml(post.body);
+			});
 		};
 
 		var onError = function(response) {
 			$('.error-container').html("<div class='messages error'>Could not fetch the data. <br /><br />More information: <br />"+response+"</div>");
 		};
 
-		var onPost = function(data) {
+		var onPost = function(data) {}
 
+		$scope.getPost = function(context, nid) {
+			context.post.shownMore = true;
+
+			backend.getPost(nid).then(function(data) {
+				context.post.body = $sce.trustAsHtml(data.article[0].body);
+			}, onError);
 		}
 
-		$scope.getPost = function(nid) {
+		$scope.showLess = function(context, nid) {
+			$log.info(context);
+
+			context.post.shownMore = false;
+
 			backend.getPost(nid).then(function(data) {
-				$scope.article = data.article[0];
-				$scope.article.body_trusted = $sce.trustAsHtml($scope.article.body);
+				context.post.body = $sce.trustAsHtml(data.summary);
 			}, onError);
 		}
 
